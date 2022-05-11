af610f6993a86189ebba7d7f5563fc5ab9ebcec0
jarumom <0k.mocogle@gmail.com>
2022-05-11 10:01:29 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,5 @@
 class BooksController < ApplicationController
   before_action :correct_user, only: [:edit,:update]
-  def new
-    @book = Book.new
-  end
-
   def index
     @book = Book.new
     @books = Book.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to books_path
+      redirect_to user_path(current_user)
     else
       render :edit
     end
